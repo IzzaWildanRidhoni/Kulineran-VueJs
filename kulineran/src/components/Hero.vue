731be05000997a101ac4a622ba1eb0a29252a572
Izza wildan ridhoni <izzawildan88@gmail.com>
2023-07-25 13:19:54 +0700

    first commit

---
+++ kulineran/src/components/Hero.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="hero">
+    <!-- desktop -->
+    <div class="d-none d-md-block">
+      <div class="row mt-4">
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h1>
+                <strong>Halal dan Lezat</strong>
+              </h1>
+              <h4> Kenikmatan Halal untuk Jiwa yang Lapar!</h4>
+              <p>Ayo segera pilih dan pesan makanan favorit Anda</p>
+              <router-link  to="/foods" class="btn btn-success btn-lg">
+                <b-icon-arrow-right></b-icon-arrow-right> Pesan
+              </router-link>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <img src="../assets/images/hero.png" width="100%" />
+        </div>
+      </div>
+    </div>
+
+    <!-- mobile -->
+    <div class="d-sm-block d-md-none">
+      <div class="row mt-4">
+        <div class="col-md-6 mb-3">
+          <img src="../assets/images/hero.png" width="100%" />
+        </div>
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h2>
+                <strong>Delicious Food Menu, </strong><br />
+                in Your Gadget
+              </h2>
+              <p>Ayo segera pilih dan pesan makanan favorit Anda</p>
+              <button class="btn btn-success btn-lg">
+                <b-icon-arrow-right></b-icon-arrow-right> Pesan
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Hero",
+};
+</script>
+
+<style>
+</style>
---
+++ kulineran/src/views/PesananSukses.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="pesanan-sukses">
+  <Navbar/>
+  <div class="container">
+    <div class="row justify-content-center mt-5">
+        <div class="col text-center">
+            <img src="../assets/images/menunggu.png" alt="" width="300px">
+            <h2><strong>Sukses !!!</strong> </h2>
+            <h4>Pesanan Anda Segera Diproses <br> Selamat Menunggu</h4>
+             <router-link class="btn btn-success mt-3" to="/">Back To Home</router-link>
+        </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+export default {
+    nama:"PesananSukses",
+    components:{
+        Navbar
+    }
+}
+</script>
+
+<style>
+
+</style>
